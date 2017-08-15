3de2fd4ac310bf5aabfe361cc1745431806b6fc5
Kevin Long <kevin.long@secure.haloprivacy.com>
2017-08-14 22:11:43 -0700

    added primes, infite and compress examples

--- 07primes.else.py
+++ 07primes.else.py
@@ -4,6 +4,6 @@ for n in range(2, upto + 1):
     for divisor in range(2, n):
         if n % divisor == 0:
             break
-    else:
+    else:  # interesting using of else at the same level as for loop not the if statement
         primes.append(n)
 print(primes)
